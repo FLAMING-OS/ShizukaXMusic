9c6f32b9c10fe1a146ffaf70971203276dc698a5
FLAMING-OS <128779626+FLAMING-OS@users.noreply.github.com>
2023-03-26 11:27:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/FLAMING-OS/friendsforever431",
+    "https://github.com/FLAMING-OS/shizukaxmusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
